9e47ff03d4916a617e11a8edbe3aa019a3bd154a
Shreyans T <shreyans.tatiya@gmail.com>
2024-04-11 14:59:57 +0530

    Add: Authroutes.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 
@@ -11,5 +12,7 @@ app.get("/", (req, res) => {
     // root route http://localhost:5000/
     res.send("Hello World!");
 });
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
