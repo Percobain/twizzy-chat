93379c2474669962c6214963c7fe2e9bafa2e80e
Shreyans T <shreyans.tatiya@gmail.com>
2024-05-02 00:20:15 +0530

    Update Get Message Endpoints.

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -42,3 +42,30 @@ export const sendMessage = async (req, res) => {
         res.status(500).json({ error: "Internal Server Error" });
     }
 }
+
+export const getMessages = async (req, res) => {
+
+	try {
+		const { id: userToChatId } = req.params;
+		const senderId = req.user._id;
+
+		const conversation = await Conversation.findOne({
+			participants: { $all: [senderId, userToChatId] },
+		}).populate("messages");   // Not reference but actual messages
+
+		if(!conversation) {
+			return res.status(200).json([]);
+		}
+
+		const messages = conversation.messages;
+
+		res.status(200).json(messages);
+
+
+
+	} catch (error) {
+		console.log("Error in sendMessage controller", error.message);
+        res.status(500).json({ error: "Internal Server Error" });
+	}
+
+}
--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { sendMessage } from '../controllers/message.controller.js';
+import { getMessages, sendMessage } from '../controllers/message.controller.js';
 import protectRoute from '../middleware/protectRoute.js';
 
 const router = express.Router();
 
+router.get("/:id", protectRoute, getMessages);
 router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
