aa34e0dd14b9d81e38fc8729422755c074bd4835
Shreyans T <shreyans.tatiya@gmail.com>
2024-05-20 16:26:26 +0530

    Update Add Shake Animation whenever you recieve a new message. Add Notification sound whever you recieve a new message.

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -12,7 +12,7 @@ const Message = ({message}) => {
 	const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
 	const bubbleBgColor = fromMe ? "bg-indigo-500" : "";
 
-	// const shakeClass = message.shouldShake ? "shake" : "";
+	const shakeClass = message.shouldShake ? "shake" : "";
 
 	return (
 		<div className={`chat ${chatClassName}`}>
@@ -21,7 +21,7 @@ const Message = ({message}) => {
 					<img alt='Tailwind CSS chat bubble component' src={profilePic} />
 				</div>
 			</div>
-			<div className={`chat-bubble text-white ${bubbleBgColor} pb-2`}>{message.message}</div>
+			<div className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}>{message.message}</div>
       <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>{formattedTime}</div>   
     </div>
   )
--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -2,12 +2,17 @@ import React, { useEffect } from 'react'
 import { useSocketContext } from '../context/SocketContext'
 import { useConversation } from '../zustand/useConversation'
 
+import notificationSound from '../assets/sounds/notification1.mp3'
+
 const useListenMessages = () => {
     const {socket} = useSocketContext()
     const {messages, setMessages} = useConversation();
 
     useEffect(() => {
         socket?.on("newMessage", (newMessage) => {
+			newMessage.shouldShake = true;
+            const sound = new Audio(notificationSound);
+            sound.play();
             setMessages([...messages, newMessage])
         })
 
