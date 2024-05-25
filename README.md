4df526c2a556af3df605ce1d5696c896f8b3982f
Shreyans T <shreyans.tatiya@gmail.com>
2024-05-25 21:47:21 +0530

    Fix: MessageInput.jsx

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MERN Stack Project: Build and Deploy a Real Time Chat App | JWT, Socket.io
+# Twizzy Chat
 
 ![Demo](demo.png)
 
--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -1,34 +1,45 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { BsSend } from 'react-icons/bs';
+import { useState } from "react";
+import { BsSend } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import useSendMessage from "../../hooks/useSendMessage";
 
-const MessageInput = ({ handleSubmit, message, setMessage, loading }) => {
-  return (
-    <form className='px-4 my-3' onSubmit={handleSubmit}>
-      <div className='w-full relative flex items-center'>
+const MessageInput = () => {
+	const [message, setMessage] = useState("");
+	const { loading, sendMessage } = useSendMessage();
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		if (!message) return;
+		await sendMessage(message);
+		setMessage("");
+	};
+
+	return (
+		<form className='px-4 my-3' onSubmit={handleSubmit}>
+			<div className='w-full relative flex'>
         <Link to="/login" className='mr-4'>
-        <button className="btn btn-ghost p-0 ">
+          <button type="button" className="btn btn-ghost p-0">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="white" className="w-5 h-5">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
             </svg>
           </button>
         </Link>
-        <input 
-          type='text' 
-          className='border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white'
-          placeholder='Send a message'
-          value={message}
-          onChange={e => setMessage(e.target.value)}
-        />
-        <button type='submit' className='absolute inset-y-0 end-0 flex items-center pe-3'>
-          {loading ? <div className='loading loading-spinner'></div> : <BsSend />}
-        </button>
-      </div>
-    </form>
-  );
+				<input
+					type='text'
+					className='border text-sm rounded-lg block w-full p-2.5  bg-gray-700 border-gray-600 text-white'
+					placeholder='Send a message'
+					value={message}
+					onChange={(e) => setMessage(e.target.value)}
+				/>
+				<button type='submit' className='absolute inset-y-0 end-0 flex items-center pe-3'>
+					{loading ? <div className='loading loading-spinner'></div> : <BsSend />}
+				</button>
+			</div>
+		</form>
+	);
 };
-
 export default MessageInput;
+
 
 
 
@@ -55,3 +66,6 @@ export default MessageInput;
 // }
 
 // export default MessageInput
+
+
+
