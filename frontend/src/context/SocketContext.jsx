c6fe77d2b447778e9dc7ed10cc81800d12b5767d
Shreyans T <shreyans.tatiya@gmail.com>
2024-05-25 16:44:33 +0530

    Fix: Socket.io Add Favicon.

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io('https://twizzy-chat.onrender.com/', {
+			const socket = io('https://www.twizzychat.xyz/', {
 				query: {
 					userId: authUser._id,
 				},
